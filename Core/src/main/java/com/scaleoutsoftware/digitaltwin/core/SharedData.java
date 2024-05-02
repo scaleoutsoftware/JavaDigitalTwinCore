c48ff4665ccbdf50798029d5ba8a30e21ecee9ae
Brandon <ripleyb@users.noreply.github.com>
2024-05-01 18:19:25 -0700

    Update javadoc

--- Core/src/main/java/com/scaleoutsoftware/digitaltwin/core/SharedData.java
+++ Core/src/main/java/com/scaleoutsoftware/digitaltwin/core/SharedData.java
@@ -3,18 +3,22 @@ package com.scaleoutsoftware.digitaltwin.core;
 public interface SharedData {
     /**
      * Retrieves an existing object from the cache.
+     * @param key the key mapping to a value.
      * @return A cache result.
      */
     public CacheResult get(String key);
 
     /**
      * Put a new key/value mapping into the cache.
+     * @param key the key mapping to a value.
+     * @param value the value.
      * @return a cache result.
      */
     public CacheResult put(String key, byte[] value);
 
     /**
      * Remove a key/value mapping from the cache.
+     * @param key the key mapping to a value.
      * @return a cache result.
      */
     public CacheResult remove(String key);
