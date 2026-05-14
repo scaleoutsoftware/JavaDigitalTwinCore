d068f76a16fc4e0f070a0370245c9a77be050c12
Brandon <ripleyb@users.noreply.github.com>
2026-05-14 10:41:09 -0700

    Removing old docs

--- docs/package-search-index.js
+++
@@ -1,1 +0,0 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"com.scaleoutsoftware.digitaltwin.core"},{"l":"com.scaleoutsoftware.digitaltwin.development"}];updateSearchResults();
